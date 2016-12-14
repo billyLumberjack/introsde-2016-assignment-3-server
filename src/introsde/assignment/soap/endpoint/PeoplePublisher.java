99f7a6361ab31a82cfb3176af309d01251bb4cdb
samuele.malavasi <samuele.malavasi@gmail.com>
2016-12-14 12:15:52 +0100

    test

--- src/introsde/assignment/soap/endpoint/PeoplePublisher.java
+++ src/introsde/assignment/soap/endpoint/PeoplePublisher.java
@@ -7,23 +7,27 @@ import java.io.IOException;
 import javax.xml.ws.Endpoint;
 
 public class PeoplePublisher {
-    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
-        String PROTOCOL = "http://";
-        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
-        if (HOSTNAME.equals("127.0.0.1"))
-        {
-            HOSTNAME = "localhost";
-        }
-        String PORT = "6902";
-        String BASE_URL = "/ws/people";
-
-        if (String.valueOf(System.getenv("PORT")) != "null"){
-            PORT=String.valueOf(System.getenv("PORT"));
-        }
-        
-        String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
-        System.out.println("Starting People Service...");
-        System.out.println("--> Published. Check out "+endpointUrl+"?wsdl");
-        Endpoint.publish(endpointUrl, new PeopleImpl());
-    }
+	public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException {
+		String PROTOCOL = "http://";
+		String HOSTNAME = "127.0.0.1";//InetAddress.getLocalHost().getHostAddress();
+		String PORT = "6902";
+		String BASE_URL = "/ws/people";
+		
+		if (HOSTNAME.equals("127.0.0.1")) {
+			HOSTNAME = "localhost";
+		}
+		
+		if (String.valueOf(System.getenv("PORT")) != "null") {
+			PORT = String.valueOf(System.getenv("PORT"));
+		}
+		
+		String endpointUrl = PROTOCOL + HOSTNAME + ":" + PORT + BASE_URL;
+		
+		System.out.println("Starting People Service...");
+		System.out.println("--> Service published. Check out at the following endpoint url:\n" 
+				+ endpointUrl + "?wsdl");
+		
+		Endpoint.publish(endpointUrl, new PeopleImpl());
+	}
 }
+
